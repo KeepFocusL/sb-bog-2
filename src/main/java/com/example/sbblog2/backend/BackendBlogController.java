f403c713900fe4dd88cc0eec4aa7ce2edeb9c3cf
KeepFocusL <1281438594@qq.com>
2025-03-17 13:53:58 +0800

    @PostMapping("add") 真正处理、真正接收用户输入的博客信息

--- src/main/java/com/example/sbblog2/backend/BackendBlogController.java
+++ src/main/java/com/example/sbblog2/backend/BackendBlogController.java
@@ -2,6 +2,7 @@ package com.example.sbblog2.backend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +13,12 @@ public class BackendBlogController {
     public String add(){
         return "backend/blog/add";
     }
+
+    @PostMapping("add")
+    public String save(){
+        System.out.println("接收到博客信息");
+        return "redirect:/backend";
+    }
+
+
 }
