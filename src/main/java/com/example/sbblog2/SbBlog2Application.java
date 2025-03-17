4887135199711b6237aeb6a23a60513408b6df3d
KeepFocusL <1281438594@qq.com>
2025-03-17 11:47:19 +0800

    Init

---
+++ src/main/java/com/example/sbblog2/SbBlog2Application.java
@@ -0,0 +1,13 @@
+package com.example.sbblog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBlog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBlog2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbblog2/SbBlog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbblog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBlog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
