f228a098eccfe81011a1006f85bd026f2712e2ee
KeepFocusL <1281438594@qq.com>
2025-03-17 13:55:55 +0800

    增加 Blog 实体类 @Entity

---
+++ src/main/java/com/example/sbblog2/Blog.java
@@ -0,0 +1,58 @@
+package com.example.sbblog2;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private String cover;
+
+    public Blog() {
+    }
+
+    public Blog(Long id, String title, String content, String cover) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.cover = cover;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
