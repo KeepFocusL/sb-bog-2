5ec29828c4b61d8e9407f4bae66cae5c5d7a1a6a
KeepFocusL <1281438594@qq.com>
2025-03-17 12:55:55 +0800

    增加 /blog/{id} 路由及博客详情视图，同时更新资源的链接

--- src/main/java/com/example/sbblog2/BlogController.java
+++ src/main/java/com/example/sbblog2/BlogController.java
@@ -2,6 +2,7 @@ package com.example.sbblog2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BlogController {
 
     @GetMapping("")
-    public String list(){
+    public String list() {
         return "blog/list";
+    }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable String id) {
+        System.out.println("id = " + id);
+        return "blog/show";
     }
 }
