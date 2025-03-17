5f1830ef552232c7742c4999502452640fef0ead
KeepFocusL <1281438594@qq.com>
2025-03-17 14:15:09 +0800

    增加后台博客删除的功能 blogRepository.deleteById(id); 使用id进行删除

--- src/main/java/com/example/sbblog2/backend/BackendBlogController.java
+++ src/main/java/com/example/sbblog2/backend/BackendBlogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +37,10 @@ public class BackendBlogController {
         model.addAttribute("blogs", blogs);
         return "backend/blog/list";
     }
-
+    @GetMapping("delete/{id}")
+    public String delete(@PathVariable Long id) {
+        blogRepository.deleteById(id);
+        return "redirect:/backend/blog";
+    }
 
 }
