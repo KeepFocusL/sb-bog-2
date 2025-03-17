b1a0aaada4c420d358c65377209cbe234032977e
KeepFocusL <1281438594@qq.com>
2025-03-17 13:51:41 +0800

    增加 /backend/blog/add 路由，显示 backend/blog/add.html

---
+++ src/main/java/com/example/sbblog2/backend/BackendBlogController.java
@@ -0,0 +1,15 @@
+package com.example.sbblog2.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("backend/blog")
+public class BackendBlogController {
+
+    @GetMapping("add")
+    public String add(){
+        return "backend/blog/add";
+    }
+}
