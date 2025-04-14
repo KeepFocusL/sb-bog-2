5e08d72905a052f7d0913bbeaa29169e6c557d29
KeepFocusL <1281438594@qq.com>
2025-04-14 15:26:18 +0800

    前台博客列表：从数据库获取，同时支持分页

--- src/main/java/com/example/sbblog2/BlogController.java
+++ src/main/java/com/example/sbblog2/BlogController.java
@@ -1,16 +1,40 @@
 package com.example.sbblog2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("blog")
 public class BlogController {
 
+    @Autowired
+    BlogRepository blogRepository;
+
     @GetMapping("")
-    public String list() {
+    public String list(Model model,
+                       @RequestParam("page") Optional<Integer> page,
+                       @RequestParam("size") Optional<Integer> size){
+        //如果没有赋值为orElse的值,为第一页
+        int currentPage = page.orElse(1);
+        //五条一页
+        int pageSize = size.orElse(5);
+
+        Pageable pageable = PageRequest.of(currentPage - 1, pageSize, Sort.by("id").descending());
+
+        Page<Blog> pageContent = blogRepository.findAll(pageable);
+
+        model.addAttribute("page",pageContent);
         return "blog/list";
     }
 
