7144cd79d118b3616ba005975324302efdc3bed8
KeepFocusL <1281438594@qq.com>
2025-05-22 15:10:44 +0800

    增加 Role 实体类

---
+++ src/main/java/com/example/sbblog2/entity/Role.java
@@ -0,0 +1,23 @@
+package com.example.sbblog2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String description;
+    int sort;
+}
