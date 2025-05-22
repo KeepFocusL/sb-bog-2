fe121e5226651a2ea47f541e49a5c5a76340da28
KeepFocusL <1281438594@qq.com>
2025-05-22 14:17:39 +0800

    撤销 d55a623d 查看被导入配置中的所有类名

--- src/main/java/com/example/sbblog2/SbBlog2Application.java
+++ src/main/java/com/example/sbblog2/SbBlog2Application.java
@@ -8,11 +8,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SbBlog2Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SbBlog2Application.class, args);
-		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-		for (String beanDefinitionName : beanDefinitionNames) {
-			System.out.println(beanDefinitionName);
-		}
+		SpringApplication.run(SbBlog2Application.class, args);
+
+        /*ConfigurableApplicationContext applicationContext = SpringApplication.run(SbBlog2Application.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }*/
 	}
 
 }
