8f5a653042c04b4f4d81b23e337acd221ed63eaf
KeepFocusL <1281438594@qq.com>
2025-05-22 15:17:37 +0800

    完善 UserRepositoryTest 单元测试...
    - 插入之后再去查询

--- src/test/java/com/example/sbblog2/dao/UserRepositoryTest.java
+++ src/test/java/com/example/sbblog2/dao/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.sbblog2.dao;
 
 import com.example.sbblog2.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,10 +20,15 @@ public class UserRepositoryTest {
     void xxx(){
 
         User user = new User();
-        user.setName("测试 - 密码加密");
+        String name = "测试2 - 查询";
+        user.setName(name);
         user.setPassword(passwordEncoder.encode("123456"));
         user.setEnabled(true);
 
         userRepository.save(user);
+
+        // 通过 UserRepository 查询名称为 "XXX" 是否存在于数据库中
+        User userDb = userRepository.findFirstByName(name);
+        Assertions.assertNotNull(userDb);
     }
 }
