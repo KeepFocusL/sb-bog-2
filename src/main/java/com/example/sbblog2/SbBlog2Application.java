d55a623db14ea6cdcfaa17b7de07d2287e4b633b
KeepFocusL <1281438594@qq.com>
2025-05-22 11:40:07 +0800

    查看被导入配置中的所有类名
    使用 ConfigurableApplicationContext applicationContext = SpringApplication.run(SbBlog2Application.class, args); 可以去查看被加入容器的所有的类

--- src/main/java/com/example/sbblog2/SbBlog2Application.java
+++ src/main/java/com/example/sbblog2/SbBlog2Application.java
@@ -2,12 +2,17 @@ package com.example.sbblog2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbBlog2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbBlog2Application.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SbBlog2Application.class, args);
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println(beanDefinitionName);
+		}
 	}
 
 }
