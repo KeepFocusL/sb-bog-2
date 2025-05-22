e3e922cde73d60ea8d10da9e0ab22784b9960c00
KeepFocusL <1281438594@qq.com>
2025-05-22 15:17:02 +0800

    从 IoC 容器中获取 PasswordEncoder 类，对密码进行加密存储

--- src/test/java/com/example/sbblog2/dao/UserRepositoryTest.java
+++ src/test/java/com/example/sbblog2/dao/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.sbblog2.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 public class UserRepositoryTest {
@@ -11,12 +12,15 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     void xxx(){
 
         User user = new User();
-        user.setName("测试");
-        user.setPassword("123456");
+        user.setName("测试 - 密码加密");
+        user.setPassword(passwordEncoder.encode("123456"));
         user.setEnabled(true);
 
         userRepository.save(user);
