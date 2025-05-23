dcb30ee5ddae96adcdfa8b03d92b6d3f2ed70003
KeepFocusL <1281438594@qq.com>
2025-05-23 10:25:28 +0800

    在 User 中增加 @ManyToMany 注解，表示 User 和 Role 的多对多关系

--- src/main/java/com/example/sbblog2/entity/User.java
+++ src/main/java/com/example/sbblog2/entity/User.java
@@ -1,14 +1,13 @@
 package com.example.sbblog2.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -25,4 +24,7 @@ public class User {
     private String phone;
     private LocalDateTime createdAt;
     private boolean enabled;
+
+    @ManyToMany
+    Set<Role> roles = new HashSet<>();
 }
