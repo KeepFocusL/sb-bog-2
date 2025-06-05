b824a8a31ce9e94f630ee3a463aa57e67e76809f
KeepFocusL <1281438594@qq.com>
2025-06-05 11:57:10 +0800

    邮件发送测试代码...
    - 引入 spring-boot-starter-mail 依赖
    - 用 qq 的邮件发送服务

---
+++ src/main/java/com/example/sbblog2/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.example.sbblog2.controller;
+
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @Autowired
+    JavaMailSender sender;
+
+    @GetMapping("send-mail")
+    String send() throws Exception {
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom(new InternetAddress("1281438594@qq.com", "客服"));
+        helper.setSubject("忘记密码 Subject");
+        helper.setTo("1281438594@qq.com");
+        helper.setText("邮件发生成功 - 邮件正文");
+
+        sender.send(message);
+
+        return "200";
+    }
+}
