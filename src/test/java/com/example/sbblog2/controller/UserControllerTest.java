a30f580b7a7e6aa96fd3947c4f112738d6ed374e
KeepFocusL <1281438594@qq.com>
2025-06-05 11:15:05 +0800

    测试不报错也打印消息 - andDo(MockMvcResultHandlers.print())

--- src/test/java/com/example/sbblog2/controller/UserControllerTest.java
+++ src/test/java/com/example/sbblog2/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
@@ -33,6 +34,7 @@ public class UserControllerTest {
                         .param("name", "admin")
                         .param("password", "password")
                 )
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.model().attributeHasFieldErrorCode("user", "email", "exist"))
         ;
     }
