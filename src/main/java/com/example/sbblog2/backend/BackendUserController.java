f5be73ef347e65a685219d37c527fa6b3a7c0a2b
KeepFocusL <1281438594@qq.com>
2025-06-05 14:19:22 +0800

    后续的思路按照注释的形式展示出来

--- src/main/java/com/example/sbblog2/backend/BackendUserController.java
+++ src/main/java/com/example/sbblog2/backend/BackendUserController.java
@@ -10,6 +10,10 @@ public class BackendUserController {
 
     @GetMapping("")
     public String list(){
+        // 找数据库要数据，由于不建议跨层所以找 Service 要对应的数据
+        // 由于需要 Service 找 IoC 容器拿(从 IoC 容器中注入)
+        // 拿到数据后返回给前端
+        // 前端拿到数据后按照需要的方式展示数据
         return "backend/user/list";
     }
 }
