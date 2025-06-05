3e290138e2593b2813ff5a96c181c2e2b031198f
KeepFocusL <1281438594@qq.com>
2025-06-05 11:11:40 +0800

    增加测试方法 userDashboardWithLogin - 验证已经登录的情况...
    - 添加 spring-security-test 依赖
    -  @WithUserDetails
    - .andExpect(MockMvcResultMatchers.status().isOk())

--- src/test/java/com/example/sbblog2/controller/UserControllerTest.java
+++ src/test/java/com/example/sbblog2/controller/UserControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.sbblog2.controller;
 
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,9 +25,13 @@ public class UserControllerTest {
     }
 
     @Test
-    @DisplayName("未登录的情况下访问 /user/dashboard 会被自动跳转到 /login")
-    void userDashboardWithoutLogin() throws Exception {
+    @WithUserDetails(userDetailsServiceBeanName = "jpaUserDetailsService" , value = "user@example.com")
+    void userDashboardWithLogin() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/user/dashboard"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("个人资料")))
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("user@example.com")))
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("用户名")))
+        ;
     }
 }
