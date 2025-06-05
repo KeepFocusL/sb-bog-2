4cc99cd285d503f7ec50a62c0b596d685dba97f8
KeepFocusL <1281438594@qq.com>
2025-06-05 14:44:20 +0800

    申请开通博客 - 增加 UserUtils 工具类，提供 getCurrentUser() 快速获取当前登录用户

---
+++ src/main/java/com/example/sbblog2/util/UserUtils.java
@@ -0,0 +1,21 @@
+package com.example.sbblog2.util;
+
+import com.example.sbblog2.bean.SecurityUser;
+import com.example.sbblog2.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class UserUtils {
+    public static SecurityUser getCurrentSecurityUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return (SecurityUser) authentication.getPrincipal();
+        }
+        return null;
+    }
+
+    public static User getCurrentUser() {
+        SecurityUser currentSecurityUser = getCurrentSecurityUser();
+        return currentSecurityUser == null ? null : currentSecurityUser.getUser();
+    }
+}
