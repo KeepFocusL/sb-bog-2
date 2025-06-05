c788e5943ec83a931e790ea440ff342cf1d3006e
KeepFocusL <1281438594@qq.com>
2025-06-05 09:53:38 +0800

    Method Security
    @EnableMethodSecurity
    
    @PreAuthorize("isAuthenticated()")
    
    https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html

--- src/main/java/com/example/sbblog2/controller/UserController.java
+++ src/main/java/com/example/sbblog2/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.sbblog2.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
+@PreAuthorize("isAuthenticated()")
 public class UserController {
 
     @GetMapping("dashboard")
