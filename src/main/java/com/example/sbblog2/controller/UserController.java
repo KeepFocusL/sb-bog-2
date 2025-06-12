413973807574ec39e0fd4d7b6cebaa87b669c203
KeepFocusL <1281438594@qq.com>
2025-06-12 11:02:12 +0800

    重置密码 - 前->C - 增加对应修改密码 form 表单对应的 POST 方法 userService.updatePassword(currentUser);

--- src/main/java/com/example/sbblog2/controller/UserController.java
+++ src/main/java/com/example/sbblog2/controller/UserController.java
@@ -211,5 +211,34 @@ public class UserController {
         model.addAttribute("changePasswordDTO", changePasswordDTO);
         return "user/change-password";
     }
+    @PostMapping("change-password")
+    @PreAuthorize("isAuthenticated()")
+    String changePassword(@Valid @ModelAttribute("changePasswordDTO") ChangePasswordDTO changePasswordDTO,
+                          BindingResult result,
+                          RedirectAttributes attributes, HttpServletRequest request) throws ServletException {
+        User currentUser = UserUtils.getCurrentUser();
 
+        // 验证当前密码是否正确
+        if (!userService.checkPassword(currentUser, changePasswordDTO.getCurrentPassword())) {
+            result.rejectValue("currentPassword", "error-current-password", "当前密码不正确");
+        }
+
+        // 验证两次新密码是否一致
+        if (!changePasswordDTO.getPassword().equals(changePasswordDTO.getConfirmPassword())) {
+            result.rejectValue("password", "error-confirm-password", "(新密码)和(确认密码)输入不一致");
+        }
+
+        if(result.hasErrors()){
+            return "user/change-password";
+        }
+
+        // 更新密码
+        assert currentUser != null;
+        currentUser.setPassword(changePasswordDTO.getPassword());
+        userService.updatePassword(currentUser);
+
+        attributes.addFlashAttribute("msg", "密码修改成功！(安全起见，有重要信息变更时，需主动登录)");
+        request.logout();
+        return "redirect:/login";
+    }
 }
